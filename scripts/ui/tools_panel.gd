aecb0702398102582d0be06c1b6f96245c0f3751
Dany <danielgeorgewadea@gmail.com>
2026-03-04 11:04:02 +0200

    Add theme, assets, & ToolManager script for making a tool selection panel
    Introduce UI support and a basic tool selection system: add UI assets; add a Theme resource and ToolsPanel scene; add GameScreen UI scene and a test scene that spawns the player and UI for testing the ToolsPanel UI. Add a global ToolManager script and select it as an autoload, and add a new input named "release_tool". Apply related adjustments in player script to integrate the tool selection system.

---
+++ scripts/ui/tools_panel.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
+@onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
+@onready var tool_watering: Button = $MarginContainer/HBoxContainer/ToolWatering
+@onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
+@onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
+
+func _on_tool_axe_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.AxeWood)
+	tool_axe.grab_focus()
+
+func _on_tool_tilling_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.TillGround)
+	tool_tilling.grab_focus()
+
+func _on_tool_watering_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.WaterCrops)
+	tool_watering.grab_focus()
+
+func _on_tool_corn_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantCorn)
+	tool_corn.grab_focus()
+
+func _on_tool_tomato_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantTomato)
+	tool_tomato.grab_focus()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("release_tool"):
+		ToolManager.select_tool(DataTypes.Tools.None)
+		tool_axe.release_focus()
+		tool_tilling.release_focus()
+		tool_watering.release_focus()
+		tool_corn.release_focus()
+		tool_tomato.release_focus()
